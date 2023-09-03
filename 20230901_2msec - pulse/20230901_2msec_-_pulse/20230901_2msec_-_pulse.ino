7fb4abac167a93d7c6626970d5b7ef0dd0b7dafc
dnjsdbqls18 <126652295+dnjsdbqls18@users.noreply.github.com>
2023-09-04 04:31:46 +0900

    Add files via upload

---
+++ 20230901_2msec - pulse/20230901_2msec_-_pulse/20230901_2msec_-_pulse.ino
@@ -0,0 +1,22 @@
+#include <MsTimer2.h>
+
+const int pulsePin = 18;
+
+void Pulse()
+{
+  static boolean pulseState = LOW;
+  digitalWrite(pulsePin, pulseState);
+  pulseState = !pulseState;
+}
+
+void setup()
+{
+  pinMode(pulsePin, OUTPUT);
+  MsTimer2::set(1.9, Pulse);
+  MsTimer2::start();
+}
+
+void loop()
+{
+
+}
